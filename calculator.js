bfc52636ed9ac75a6e8384939ee0a427d67c386c
kuimercy <wanjirumercy2000@gmail.com>
2024-07-19 14:36:43 +0300

    implement arithmetic operations and add files

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+
+let result = multiply(4,4);
+console.log(result);
